03c3160c16811e70f8826d9cd60b920da10e139a
marcelobojikian <marcelo.bojikian@gmail.com>
2022-08-03 15:03:28 +0100

    Configuracao inicial do projeto

---
+++ src/test/java/br/com/alura/challenge/finance/ControleFinanceiroApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.alura.challenge.finance;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinanceiroApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
